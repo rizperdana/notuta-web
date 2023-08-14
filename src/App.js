48e82269e67dac5a49966b1a63dcdd829da2e80b
Rizki Perdana <perdana.rizki16@gmail.com>
2023-08-14 23:45:19 +0700

    create app nav

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Link, BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import AddTodo from "./components/add-todo.component";
+import Todo from "./components/todo.component";
+import TodoList from "./components/todo-list.component";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <Link to={"/todos"} className="navbar-brand">
+            Notuta
+          </Link>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/todos"} className="nav-link">
+                Todos
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/add"} className="nav-link">
+                Add
+              </Link>
+            </li>
+          </div>
+        </nav>
+
+        <div className="container mt-3">
+          <Switch>
+            <Route exact path={["/", "/todos"]} component={TodoList} />
+            <Route exact path="/add" component={AddTodo} />
+            <Route exact path="/todos/:id" component={Todo} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
